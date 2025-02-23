ee51eb8d4b754079d4f3455df3d7e6ee6f8028ba
Shariq Choudhary <khanshariq92213@gmail.com>
2025-02-23 20:32:32 +0530

    Minir changes

--- controllers/quran.js
+++ controllers/quran.js
@@ -5,34 +5,47 @@ import { quran } from "../data/quran.js";
 
 export const getAyahBySurahNumberAndVerseNumber = async (req, res) => {
   const { ayah } = req.params;
+  const { lang } = req.query; // Extract language from query params
 
   if (!ayah) {
-    return res
-      .status(400)
-      .json({
-        code: 400,
-        status: "Error",
-        message: "Missing ayah parameter. Use '/surahNumber:verseNumber'",
-      });
+    return res.status(400).json({
+      code: 400,
+      status: "Error",
+      message: "Missing ayah parameter. Use '/surahNumber:verseNumber'",
+    });
   }
 
   const [surahNumber, verseNumber] = ayah.split(":");
 
   if (!surahNumber || !verseNumber) {
-    return res
-      .status(400)
-      .json({
-        code: 400,
-        status: "Error",
-        message: "Invalid ayah format. Use 'surahNumber:verseNumber'",
-      });
+    return res.status(400).json({
+      code: 400,
+      status: "Error",
+      message: "Invalid ayah format. Use 'surahNumber:verseNumber'",
+    });
   }
 
-  const url = `http://api.alquran.cloud/v1/ayah/${surahNumber}:${verseNumber}/ar.alafasy`;
+  // Determine the translation identifier based on the requested language
+  let translation;
+  switch (lang) {
+    case "arabic":
+      translation = "ar.alafasy";
+      break;
+    case "eng":
+      translation = "en.asad";
+      break;
+    case "urdu":
+      translation = "ur.junagarhi";
+      break;
+    default:
+      translation = "ar.alafasy"; // Default to Arabic if no valid language is provided
+  }
+
+  const url = `http://api.alquran.cloud/v1/ayah/${surahNumber}:${verseNumber}/${translation}`;
 
   try {
     const response = await axios.get(url);
-    const data = response.data; // axios already parses the JSON
+    const data = response.data;
 
     if (data.code === 200 && data.status === "OK") {
       // Add the dynamic image URL to the response
@@ -45,15 +58,14 @@ export const getAyahBySurahNumberAndVerseNumber = async (req, res) => {
     }
   } catch (error) {
     console.error("Error fetching Quran data:", error);
-    res
-      .status(500)
-      .json({
-        code: 500,
-        status: "Error",
-        message: "Failed to fetch Quran data",
-      });
+    res.status(500).json({
+      code: 500,
+      status: "Error",
+      message: "Failed to fetch Quran data",
+    });
   }
 };
+
 
 export const getSurahs = async (req, res) => {
   const url = "http://api.alquran.cloud/v1/surah";
